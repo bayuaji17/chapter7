3212bc70ab3f48b3b3c701229437bf6737e3b74b
bayuaji17 <nugroho.bayuaji17@gmail.com>
2023-11-14 21:34:01 +0700

    chapter 7

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { ParticleBackground } from "./components/ParticleBackground";
+
+function App() {
+  return (
+    <>
+      <ParticleBackground />
+      <h1 className="myName">Bayu Aji Nugroho</h1>
+      <p className="kelas">FEJS-2</p>
+    </>
+  );
+}
+
+export default App;
