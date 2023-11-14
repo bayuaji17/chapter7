848b6d615ccb41de876175a1213501ce97c89f0a
bayuaji17 <nugroho.bayuaji17@gmail.com>
2023-11-14 21:40:13 +0700

    fix bug

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
 import './assets/The-Impostor.woff2'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
